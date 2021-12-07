f9327665f69825340c2bec4b4c0d51471e93ecaa
Colin <colin.mcnelis3@gmail.com>
2021-12-07 20:33:13 +0000

    Project Backup

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
 # Reads Fleet Data CSV
-df = pd.read_csv("fleet.csv", index_col=0)
-df.columns.tolist()
+df = pd.read_csv("fleet.csv", index_col=0, skipinitialspace=True)
+
 # Take a look to understand data
 print(df.head())
 print(df.shape)
@@ -23,5 +23,17 @@ print(df.shape)
 Lessor = df.sort_values(by=['Lessor/Owner', 'Operator Country/Territory', 'Age'])
 print(Lessor.head())
 
-df1 = df.iloc[0:100, 1:5]
+# Using loc to create subset of Dataframe examining EasyJet Fleet
+df1 = df.loc[["easyJet"],["Status", "Aircraft Variant"]]
 print(df1.shape)
+
+# Using loc to create subset of Dataframe examining Lessor Portfolio
+df2 = df.loc[:,["Lessor/Owner", "Aircraft Variant", "Age"]]
+print(df2.shape)
+
+df3 = df.loc[["Wizz Air"],["Aircraft Variant", "Age"]]
+
+df4 = df.iloc[[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], [0, 10]]
+print(df4)
+
+print(df["Age"])
